5244ed1a0eb147162951ef464e1eb26323539a09
amodioandrea <92790243+amodioandrea@users.noreply.github.com>
2021-11-14 22:27:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+
+int n;
+cin>>n;
+if(n>=2) {
+
+    int x = 0, y = 1, z;
+    for (int i; i <n; i++) {
+
+        z = x + y;
+        x = y;
+        y = z;
+        cout << x<<endl;
+    }
+}else{
+        cout<<"errore";
+
+}
+    return 0;
 }
